1aa4d704c637758ea4ab0741e5ce59b11cc71a99
Star20003 <stateira.un@gmail.com>
2023-07-22 09:25:40 +0430

    js homework session 1

---
+++ js_hw_01.js
@@ -0,0 +1,9 @@
+// JS session 1 
+
+let firstNum = +prompt('Enter your first number:')
+let secondNum = +prompt('Enter your second number:')
+
+let result = firstNum + secondNum
+
+console.log('The summation of your numbers:', result)
+document.writeln('You can see the result in console section')
